bafe902280ceb9a90c4f9a54a13904f63cb1a6d1
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-26 04:26:40 -0600

    implement controller ProductController

---
+++ src/main/java/com/exception_handling/ProductController.java
@@ -0,0 +1,16 @@
+package com.exception_handling;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProductController {
+
+  @RequestMapping(value = "/home", method = RequestMethod.GET)
+  public ResponseEntity<Object> getListProduct() {
+    throw DuplicateEntity.product("nintendo switch");
+  }
+
+}
