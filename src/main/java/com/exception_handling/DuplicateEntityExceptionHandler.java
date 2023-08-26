043b11841ca328510c852c02dbad2ecb75b5570d
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-26 04:26:25 -0600

    implement handler-controller DuplicateEntityExceptionHandler

---
+++ src/main/java/com/exception_handling/DuplicateEntityExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.exception_handling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class DuplicateEntityExceptionHandler {
+
+  @ExceptionHandler(value = DuplicateEntity.class)
+  public ResponseEntity<Object> exception(DuplicateEntity exception) {
+    final var body = Map.of(
+      "message", exception.getMessage(),
+      "entity", exception.getEntityName(),
+      "duplicatedId", exception.getEntityId()
+    );
+    return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+  }
+
+}
