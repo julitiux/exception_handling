ea433733326b5c245d1a9bc3cdbb1d44e94859fc
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-29 22:40:11 -0600

    rename controller ProductController -> UserController

--- src/main/java/com/exception_handling/UserController.java
+++ src/main/java/com/exception_handling/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class ProductController {
+public class UserController {
 
   @RequestMapping(value = "/home", method = RequestMethod.GET)
   public ResponseEntity<Object> getListProduct() {
