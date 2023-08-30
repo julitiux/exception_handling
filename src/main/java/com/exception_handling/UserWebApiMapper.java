8f843eb89ac8201dbba80e08051d2b4593bf7385
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-29 22:53:15 -0600

    implement method of; return a object UserDto

--- src/main/java/com/exception_handling/UserWebApiMapper.java
+++ src/main/java/com/exception_handling/UserWebApiMapper.java
@@ -1,9 +1,15 @@
 package com.exception_handling;
 
+import java.util.Objects;
+
 public class UserWebApiMapper {
 
-  public static User of(final AddUserCommand addUserCommand){
+  public static User of(final AddUserCommand addUserCommand) {
     return new User(null, addUserCommand.username(), addUserCommand.password(), null);
+  }
+
+  public static UserDto of(final User user) {
+    return new UserDto(Objects.requireNonNull(user.id).toString(), user.username);
   }
 
 }
