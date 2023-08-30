12edc1248167b918eea489b33aebeb6c4781a4fb
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-29 23:40:13 -0600

    add @RequestBody tag

--- src/main/java/com/exception_handling/UserController.java
+++ src/main/java/com/exception_handling/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,7 +17,7 @@ public class UserController {
   UserService userService;
 
   @RequestMapping(value = "/user", method = RequestMethod.POST)
-  public ResponseEntity<UserDto> addUser(AddUserCommand addUserCommand) {
+  public ResponseEntity<UserDto> addUser(@RequestBody AddUserCommand addUserCommand) {
     var saved = userService.addUser(UserWebApiMapper.of(addUserCommand));
     if (Objects.nonNull(saved.id))
       return new ResponseEntity<>(UserWebApiMapper.of(saved), HttpStatus.CREATED);
