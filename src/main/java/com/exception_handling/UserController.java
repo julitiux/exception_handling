aebf60b544c90abb018d6dafcb4814fd68343156
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-29 23:01:50 -0600

    implement again the UserController

--- src/main/java/com/exception_handling/UserController.java
+++ src/main/java/com/exception_handling/UserController.java
@@ -1,16 +1,27 @@
 package com.exception_handling;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Objects;
+
 @Controller
 public class UserController {
 
-  @RequestMapping(value = "/home", method = RequestMethod.GET)
-  public ResponseEntity<Object> getListProduct() {
-    throw DuplicateEntity.product("nintendo switch");
+  @Autowired
+  UserService userService;
+
+  @RequestMapping(value = "/user", method = RequestMethod.POST)
+  public ResponseEntity<UserDto> addUser(AddUserCommand addUserCommand) {
+    var saved = userService.addUser(UserWebApiMapper.of(addUserCommand));
+    if (Objects.nonNull(saved.id))
+      return new ResponseEntity<>(UserWebApiMapper.of(saved), HttpStatus.CREATED);
+    else
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
   }
 
 }
