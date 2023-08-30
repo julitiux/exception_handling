8ae2fd19d21dc3a5a738774390043da7ce7441d3
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-29 22:18:42 -0600

    implement repository UserRepository

---
+++ src/main/java/com/exception_handling/UserRepository.java
@@ -0,0 +1,12 @@
+package com.exception_handling;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, UUID> {
+  Optional<User> findByUsername(final String username);
+}
