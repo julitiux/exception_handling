42bb303042292eb830a9ec5e4fcae36b7989d177
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-30 23:36:40 -0600

    changing the ExceptionHandler DataAccessResourceFailureException.class -> DataAccessException.class

--- src/main/java/com/exception_handling/DataAccessResourceExceptionHandler.java
+++ src/main/java/com/exception_handling/DataAccessResourceExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.exception_handling;
 
-import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,8 +11,8 @@ import java.util.Map;
 @ControllerAdvice
 public class DataAccessResourceExceptionHandler {
 
-  @ExceptionHandler(value = DataAccessResourceFailureException.class)
-  public ResponseEntity<Object> exception(DataAccessResourceFailureException exception) {
+  @ExceptionHandler(value = DataAccessException.class)
+  public ResponseEntity<Object> exception(DataAccessException exception) {
     final var body = Map.of(
       "message", exception.getMessage()
     );
